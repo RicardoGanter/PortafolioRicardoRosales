0e3c2f79fc8bac6ae8a46debae27921eb4873206
Ricardo Ganter <ricardorosalesganter@gmail.com>
2024-10-16 15:36:41 -0300

    adicion de url funcional de mis redes

--- src/app/(layout)/socialNetworks.tsx
+++ src/app/(layout)/socialNetworks.tsx
@@ -1,12 +1,16 @@
 import GithubIcon from "@/public/github.svg"
 import LinkedinIcon from "@/public/linkedin.svg"
 import styles from "@/styles/(layout)/socialNetworks.module.scss"
-
+import Link from "next/link"
 const SocialNetworks = ()=>{
   return(
     <div className={styles.contain}>
-      <GithubIcon width={70} className={styles.image} alt="Icono de redes sociales github."/>
-      <LinkedinIcon className={styles.image} alt="Icono de redes sociales linkedin."/>
+      <Link target="_blank" href={'https://github.com/RicardoGanter'}>
+        <GithubIcon width={70} className={styles.image} alt="Icono de redes sociales github."/>
+      </Link>
+      <Link target="_blank" href={'https://www.linkedin.com/in/ricardo-adolfo-rosales-ganter-03362b242/'}>
+        <LinkedinIcon className={styles.image} alt="Icono de redes sociales linkedin."/>
+      </Link>
     </div>
   )
 }
